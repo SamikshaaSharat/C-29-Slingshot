014e6975731920f16d33bc523bc9f92727392cc0
SamikshaaSharat <70048833+SamikshaaSharat@users.noreply.github.com>
2020-09-15 13:25:19 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,29 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+//to make the bird fly 
+    fly(){
+        this.sling.bodyA = null;
+    }
+    display(){
+        //to make the bird fly one its released 
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        
+    }
+    
+}
